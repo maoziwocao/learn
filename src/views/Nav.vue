7d1ff1e1b314fbe2bb02b7c32eba9d2c70861bc7
yaowei04361 <yaowei04361@hellobike.com>
2020-05-18 20:34:08 +0800

    feat(package): 引入pug

--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -1,11 +1,19 @@
-<template>
-  <div>
-    <router-link to='./home'>home</router-link>
-    <router-link to='./deepClone'>deepClone</router-link>
-    <router-link to='./debounce'>防抖 节流</router-link>
-    <router-link to='./component'>npm component</router-link>
-    <router-link to='./uniqueSelect'>多个select共享数据， 值唯一，其他无法选择</router-link>
-  </div>
+<template lang="pug">
+  div
+    router-link(to='./home') home
+    router-link(to='./deepClone') deepClone
+    router-link(to='./debounce') 防抖 节流
+    el-tooltip(
+      content="vue cli3 打包组件为一个npm包给别的项目使用"
+      placement="bottom"
+    )
+      router-link(to='./component') npm component
+    el-tooltip(
+      content="多个select共享数据, 值唯一，其中一个选择后其他select无法选择"
+      placement="bottom"
+    )
+      router-link(to='./uniqueSelect') uniqueSelect
+
 </template>
 
 <script>
